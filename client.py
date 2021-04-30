042defdf7075fea3084e1d63c7a98ffc09495deb
Asthakapil2020 <83447017+Asthakapil2020@users.noreply.github.com>
2021-04-30 13:11:17 +0530

    Step 1 : Creating socket

---
+++ client.py
@@ -0,0 +1,18 @@
+#importing libraries
+import socket 
+import sys
+
+#Create a socket (Connect two computers)
+
+def create_socket():
+  try:
+    
+      global host
+      global port
+      global s
+      host = " "
+      port = 9999
+      s = socket.socket()
+      
+   except socket.error as msg:
+       print("Socket creation error: " + str(msg))
